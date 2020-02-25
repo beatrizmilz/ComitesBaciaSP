338aa0136f6abaf6b03ad6032bad79e1c4c594bf
Beatriz Milz <beatriz.milz@hotmail.com>
2020-02-25 08:24:02 -0300

    Initial commit

---
+++ R/extract_file_format.R
@@ -0,0 +1,20 @@
+#' Extract file format
+#'
+#' Extract the file format from a url. If is NA, returns a NA.
+#'
+#' @param url character
+#'
+#' @return character
+#' @export
+#'
+#' @examples
+#' file <- "https://github.com/hadley/pkg-dev/blob/master/1-intro.pdf"
+#' extract_file_format(file)
+
+extract_file_format <- function(url) {
+  ifelse(
+    is.na(url),
+    NA,
+    stringr::str_extract(url, pattern = "(.doc|.docx|.pdf|.html|.htm|.jpg|.pd)$|drive.google")
+  )
+}
