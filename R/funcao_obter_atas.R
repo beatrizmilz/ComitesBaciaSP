65585622cbfd6cbbeb918a2b816240a78489356d
Beatriz Milz <beatriz.milz@hotmail.com>
2020-10-21 14:24:44 -0300

    altera argumento da funcao obter atas, cria pasta data-raw

--- R/funcao_obter_atas.R
+++ R/funcao_obter_atas.R
@@ -2,15 +2,35 @@
 #'
 #' Função para obter tabelas sobre atas dos Comitês de Bacia no Estado de São Paulo
 #'
-#' @param n_comite Número referente ao comitê. É possível verificar na base:  \code{\link{comites_sp}}.
+#' @param sigla_comite Texto referente à sigla do comitê. É possível verificar na base:  \code{\link{comites_sp}}.
 #' @return Retorna uma tibble. Uma base com dados coletados para todos os comitês está disponível em \code{\link{atas_comites}}.
 #' @export
 #'
-#' @examples obter_tabela_atas_comites(6)
-obter_tabela_atas_comites <- function(n_comite) {
+#' @examples obter_tabela_atas_comites("at")
+obter_tabela_atas_comites <- function(sigla_do_comite) {
+  siglas_dos_comites <- comites_sp %>%
+    dplyr::pull(sigla_comite) %>%
+    unique()
 
-   comite <- comites_sp %>%
-    dplyr::filter(n_ugrhi == n_comite)
+  texto_siglas_dos_comites <-   siglas_dos_comites  %>%
+    paste(collapse = ", ")
+
+  if (!sigla_do_comite %in% siglas_dos_comites) {
+    stop(
+      paste(
+        "O texto fornecido para o argumento 'sigla_do_comite' não é válido.
+        Forneça uma das seguintes possibilidades:",
+        texto_siglas_dos_comites
+      )
+    )
+  }
+
+
+  comite <- comites_sp %>%
+    dplyr::filter(sigla_comite == sigla_do_comite) %>%
+    dplyr::top_n(1, wt = n_ugrhi)
+
+  n_comite <- comite  %>% dplyr::pull(n_ugrhi)
 
   link_comite <-
     comite  %>%
@@ -43,50 +63,50 @@ obter_tabela_atas_comites <- function(n_comite) {
 
   } else{
     nome_reuniao <- lista %>%
-      purrr::map( ~  rvest::html_nodes(.x, "h2")) %>%
-      purrr::map( ~ .x[1]) %>%
-      purrr::map( ~ rvest::html_text(.x)) %>%
+      purrr::map(~  rvest::html_nodes(.x, "h2")) %>%
+      purrr::map(~ .x[1]) %>%
+      purrr::map(~ rvest::html_text(.x)) %>%
       purrr::as_vector()
 
 
     lista_dados <- lista %>%
-      purrr::map(~  rvest::html_nodes(.x, "ul"))
+      purrr::map( ~  rvest::html_nodes(.x, "ul"))
 
     tres_nodes <-
       lista_dados %>%
-      purrr::map(~  rvest::html_nodes(.x, "li"))
+      purrr::map( ~  rvest::html_nodes(.x, "li"))
 
     data_postagem <- tres_nodes  %>%
-      purrr::map( ~ .x[2])  %>%
-      purrr::map(~  rvest::html_text(.x)) %>%
-      purrr::map(~ stringr::str_extract(.x,  "[0-9]{2}/[0-9]{2}/[0-9]{4}")) %>%
+      purrr::map(~ .x[2])  %>%
+      purrr::map( ~  rvest::html_text(.x)) %>%
+      purrr::map( ~ stringr::str_extract(.x,  "[0-9]{2}/[0-9]{2}/[0-9]{4}")) %>%
       purrr::as_vector() %>%
       lubridate::as_date(format = "%d/%m/%Y")
 
 
     data_reuniao <- tres_nodes  %>%
-      purrr::map( ~ .x[1])  %>%
-      purrr::map(~  rvest::html_text(.x)) %>%
-      purrr::map(~ stringr::str_extract(.x,  "[0-9]{2}/[0-9]{2}/[0-9]{4}")) %>%
+      purrr::map(~ .x[1])  %>%
+      purrr::map( ~  rvest::html_text(.x)) %>%
+      purrr::map( ~ stringr::str_extract(.x,  "[0-9]{2}/[0-9]{2}/[0-9]{4}")) %>%
       purrr::as_vector() %>%
       lubridate::as_date(format = "%d/%m/%Y")
 
     link_ata <-
       tres_nodes %>%
-      purrr::map( ~ rvest::html_nodes(.x, "a")) %>%
-      purrr::map( ~ rvest::html_attr(.x, "href")) %>%
-      purrr::map( ~ tibble::as_tibble(.x)) %>%
-      purrr::map( ~ dplyr::mutate(.x, link_numero = paste0("ata_", dplyr::row_number(.x))))  %>%
-      purrr::map( ~ dplyr::mutate(
+      purrr::map(~ rvest::html_nodes(.x, "a")) %>%
+      purrr::map(~ rvest::html_attr(.x, "href")) %>%
+      purrr::map(~ tibble::as_tibble(.x)) %>%
+      purrr::map(~ dplyr::mutate(.x, link_numero = paste0("ata_", dplyr::row_number(.x))))  %>%
+      purrr::map(~ dplyr::mutate(
         .x,
         value = dplyr::case_when(
           stringr::str_starts(value , "/public") ~ paste0("http://www.sigrh.sp.gov.br", value),
           TRUE ~ value
         )
       )) %>%
-      purrr::map( ~ tidyr::pivot_wider(.x, values_from = value, names_from = link_numero)) %>%
-      purrr::map( ~ tibble::as_tibble(.x)) %>%
-      purrr::map( ~ if (nrow(.x) == 0) {
+      purrr::map(~ tidyr::pivot_wider(.x, values_from = value, names_from = link_numero)) %>%
+      purrr::map(~ tibble::as_tibble(.x)) %>%
+      purrr::map(~ if (nrow(.x) == 0) {
         .x %>% tibble::add_row()
       } else {
         .x
@@ -136,21 +156,3 @@ obter_tabela_atas_comites <- function(n_comite) {
   }
 
 }
-
-# View(obter_tabela_atas_comites(3))
-
-# #SALVAR OS DADOS NO PACOTE -----------------
-# numeros_dos_comites <- comites_sp %>%
-#   dplyr::filter(n_ugrhi != 21) %>% # As UGRHIs 21 e 20 tem o mesmo comitê
-#   dplyr::pull(n_ugrhi)
-#
-# atas_comites <-
-#   purrr::map_df(numeros_dos_comites, obter_tabela_atas_comites)
-#
-# atas_comites %>%
-#   write.csv2(
-#     file = glue::glue("inst/extdata/tabela_atas_comites_{Sys.Date()}.csv"),
-#     fileEncoding = "UTF-8"
-#   )
-#
-# usethis::use_data(atas_comites, overwrite = TRUE)
--- data-raw/comites_sp.R
+++ data-raw/comites_sp.R
@@ -1,3 +1,5 @@
+## code to prepare `comites_sp` dataset goes here
+
 municipios_sp <-
   xml2::read_html("http://www.sigrh.sp.gov.br/municipios") %>%
   rvest::html_table(header = TRUE) %>%
@@ -10,9 +12,11 @@ comites_sp <- municipios_sp %>%
     n_ugrhi = stringr::str_extract(ugrhi,  "[0-9]{2}"),
     sigla_comite = stringr::str_remove(cbh, "CBH-"),
     sigla_comite = stringr::str_to_lower(sigla_comite),
-    n_ugrhi = dplyr::case_when(sigla_comite == "ps" ~ 02,
-                               sigla_comite == "tj" ~ 13,
-                               TRUE ~ as.double(n_ugrhi))
+    n_ugrhi = dplyr::case_when(
+      sigla_comite == "ps" ~ 02,
+      sigla_comite == "tj" ~ 13,
+      TRUE ~ as.double(n_ugrhi)
+    )
   ) %>%
 
   dplyr::group_by(bacia_hidrografica, sigla_comite, n_ugrhi) %>%
@@ -20,15 +24,14 @@ comites_sp <- municipios_sp %>%
   dplyr::ungroup() %>%
   dplyr::arrange(n_ugrhi) %>%
   dplyr::mutate(macrometropole_daee = dplyr::case_when(
-    sigla_comite %in% c("ps","ln","pcj","at","bs","mogi","smt","rb") ~ TRUE,
+    sigla_comite %in% c("ps", "ln", "pcj", "at", "bs", "mogi", "smt", "rb") ~ TRUE,
     TRUE ~ FALSE
   )) # %>%
-  # dplyr::mutate(
-  #   links_funcionando = purrr::map_dbl(links, purrr::possibly(
-  #     ~ httr::status_code(httr::GET(.x)), otherwise = NA
-  #   ))
-  # )
+# dplyr::mutate(
+#   links_funcionando = purrr::map_dbl(links, purrr::possibly(
+#     ~ httr::status_code(httr::GET(.x)), otherwise = NA
+#   ))
+# )
 
 
-# usethis::use_data(comites_sp, overwrite = TRUE)
-
+usethis::use_data(comites_sp, overwrite = TRUE)
