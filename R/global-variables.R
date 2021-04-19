ce86cf6abd25449d0715dacd533f6a237a69c0a1
Beatriz Milz <beatriz.milz@hotmail.com>
2021-04-19 10:32:09 -0300

    resolving warnings and change licence

---
+++ R/global-variables.R
@@ -0,0 +1,2 @@
+utils::globalVariables(c(".", "bacia_hidrografica", "contains", "link_representantes", "links", "n_ugrhi", "name",
+                         "nome", "nome_1", "ordem", "posicao_blocos", "set", "sigla_comite", "value"))
