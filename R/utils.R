d005820c9e2f2cf74828597dad1f4b6552d10a54
Beatriz Milz <milz.bea@gmail.com>
2023-07-24 14:23:35 -0300

    NA_character_

--- R/utils.R
+++ R/utils.R
@@ -2,12 +2,12 @@ tratar_vazio <- function(x) {
   classe <- class(x)
   if (classe[1] == "tbl_df") {
     if (nrow(x) == 0) {
-      x <- tibble::tibble(arquivos = list(NA))
+      x <- tibble::tibble(arquivos = list(NA_character_))
     }
   } else if(length(x) == 0 ){
-    x <- NA
+    x <- NA_character_
   } else if (is.na(x) | is.null(x)) {
-    x <- NA
+    x <- NA_character_
   }
 
   x
